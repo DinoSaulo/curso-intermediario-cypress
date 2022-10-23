19d2419ba05982f4e1a4f4162642255c263a001c
sbarros <saulo.barros@capgemini.com>
2022-10-23 11:28:23 -0300

    refactor: migrating a GUI function to API function

--- cypress/integration/gui/createIssue.spec.js
+++ cypress/integration/gui/createIssue.spec.js
@@ -15,7 +15,7 @@ describe('Create Issue', () => {
 
     beforeEach(() => {
         cy.login()
-        cy.gui_createProject(issue.project)
+        cy.api_createProject(issue.project)
     })
 
     it('Successfully', () => {
