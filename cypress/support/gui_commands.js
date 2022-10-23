baef6c1ca67f80743996bba8c4d19591b0f91753
sbarros <saulo.barros@capgemini.com>
2022-10-23 18:51:38 -0300

    minor: add tests using labels

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -49,7 +49,6 @@ Cypress.Commands.add('gui_createProject', project => {
     cy.contains('Create project').click()
 })
 
-
 Cypress.Commands.add('gui_createIssue', issue => {
 
     cy.visit(`${Cypress.env('user_name')}/${issue.project.name}/issues/new`)
@@ -57,4 +56,11 @@ Cypress.Commands.add('gui_createIssue', issue => {
     cy.get('.qa-issuable-form-title').type(issue.title)
     cy.get('.qa-issuable-form-description').type(issue.description)
     cy.contains('Submit issue').click()
+})
+
+Cypress.Commands.add('gui_setLabelOnIssue', label => {
+
+    cy.get('.qa-edit-link-labels').click()
+    cy.contains(label.name).click()
+    cy.get('body').click()
 })
